3ec5c0f9adad51e4fa8384b34c6e50286df18a2b
Howie17 <Howie17@gmail.com>
2017-03-01 17:09:28 -0500

    minor json update

--- client/src/pages/DSForecast.js
+++ client/src/pages/DSForecast.js
@@ -5,13 +5,9 @@ class DSForecast extends React.Component {
     render() {
         return (
             <div className="container">
-                
+                <ol className="minutelyData">
+                </ol>
                 <button onClick={minutely}>Update Minutely</button>
-                
-                <ul className="minutely">
-                    <li>Minutely</li>
-                    <minutely />
-                </ul>
             </div>
         );
     }
@@ -19,25 +15,62 @@ class DSForecast extends React.Component {
 
 function minutely() {
     var myRequest = new Request('/api/course/Carter%20Park');
-    var hourForecast = "";
-    
+    var forecast = '';
+
     fetch(myRequest)
         .then(function(response) { return response.json(); })
         .then(function(json) {
-            var i = 0;
-            for (i = 0; i < json.minutely.data.length; i++) {
-                var listItem = '<li>The precipitation at <strong>' + json.minutely.data[i].time + '</strong> will be <strong>' + json.minutely.data[i].precipIntensity + '</strong></li>';
-                hourForecast += listItem;
-                console.log(hourForecast);
-            }
-        })
 
+            forecast = json;
+            console.log(forecast);
+            var update = '';
+            var output = '';  
+            update = document.getElementById('minutelyData');                                                              
+            for (var i = 0; i <= forecast.minutely.data.length; i++) {                              // Guide: https://www.youtube.com/watch?v=HWdLisBrlV8 array at 13:30
+                for (var key in forecast.minutely.data[i]) {
+                    if (forecast.minutely.data[i].hasOwnProperty(key)) {
+                        output += '<li>' + forecast.minutely.data[i][key] + '</li>';
+                    }   // hasOwnProperty check
+                }       //for each object
+            }           //for each array element
+
+            console.log(update)
+            update.innerHTML = output
+        })
+    
+    
+    /*
+    var output = '';                                                                // Guide: https://www.youtube.com/watch?v=HWdLisBrlV8 array at 13:30
+
+    for (var i = 0; i <= forecast.minutely.data.length; i++) {
+        for (key in forecast.minutely.data[i]) {
+            if (forecast.minutely.data[i].hasOwnProperty(key)) {
+                output += '<li>' + forecast.minutely.data[i][key] + '</li>';
+            }   // hasOwnProperty check
+        }       //for each object
+    }           //for each array element
+
+    var update = document.getElementById('minutelyData');
+    update.innerHTML = output;
+   
     return(
         <div className="minutely">
-            <ul className="data">
-                {hourForecast}
-            </ul>
+            <ol className="minutelyData">
+
+            </ol>
         </div>
     );
+     */
+
+    /*
+    var i = 0;
+    for (i = 0; i < json.minutely.data.length; i++) {
+        var listItem = '<li>The precipitation at <strong>' + json.minutely.data[i].time + '</strong> will be <strong>' + json.minutely.data[i].precipIntensity + '</strong></li>';
+        hourForecast += listItem;
+        
+    }
+    var stringForecast = JSON.stringify(hourForecast);
+    console.log(stringForecast);
+    */
 }
 export default DSForecast;
