1f96e6c59888fb0ca031c07e4370d2a911e70b98
Howie17 <Howie17@gmail.com>
2017-02-28 23:57:20 -0500

    json request working

--- client/src/pages/DSForecast.js
+++ client/src/pages/DSForecast.js
@@ -5,32 +5,39 @@ class DSForecast extends React.Component {
     render() {
         return (
             <div className="container">
-                <ul></ul>
+                
+                <button onClick={minutely}>Update Minutely</button>
+                
+                <ul className="minutely">
+                    <li>Minutely</li>
+                    <minutely />
+                </ul>
             </div>
         );
     }
 }
 
-var myList = document.querySelector('ul');
+function minutely() {
+    var myRequest = new Request('/api/course/Carter%20Park');
+    var hourForecast = "";
+    
+    fetch(myRequest)
+        .then(function(response) { return response.json(); })
+        .then(function(json) {
+            var i = 0;
+            for (i = 0; i < json.minutely.data.length; i++) {
+                var listItem = '<li>The precipitation at <strong>' + json.minutely.data[i].time + '</strong> will be <strong>' + json.minutely.data[i].precipIntensity + '</strong></li>';
+                hourForecast += listItem;
+                console.log(hourForecast);
+            }
+        })
 
-var myRequest = new Request('api/course/Carter Park.json');
-
-
-fetch(myRequest)
-    .then(function(response) { return response.json(); })
-    .then(function(json) {
-        var i = 0;
-        for (i = 0; i < json.minutely.data.length; i++) {
-            var listItem = document.createElement('li');
-            listItem.innerHTML = 'The precipitation at <strong>' + json.minutely.data[i].time + '</strong> will be <strong>' + json.minutely.data[i].precipIntensity + '</strong>';
-            myList.appendChild(listItem);
-        }
-});
-
+    return(
+        <div className="minutely">
+            <ul className="data">
+                {hourForecast}
+            </ul>
+        </div>
+    );
+}
 export default DSForecast;
-
-
-
-
-
-
