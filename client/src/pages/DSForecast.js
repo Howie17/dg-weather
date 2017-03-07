b0e3bde8c73debf32c3d34eb8d26d6ff9d2381b5
howie17 <Howie17@gmail.com>
2017-03-07 14:37:56 -0500

    Minor update: using json

--- client/src/pages/DSForecast.js
+++ client/src/pages/DSForecast.js
@@ -80,51 +80,74 @@ class DSSearch extends React.Component {
 }
 
 class DSWeekly extends React.Component {
-        /*
-        constructor(props) {
-            super(props);
-            
-        };
-    */
-    render(){
-        var dotw = "";
+    /*
+    constructor(props) {
+        super(props);
+            this.props.forecast.daily = this.props.forecast.daily.bind(this);
 
-        switch(new Date(this.props.forecast.daily.data[1].time*1000).getDay()){
-                    case 0: 
-                        dotw = "Sunday";
-                        break;
-                    case 1:
-                        dotw = "Monday";
-                        break;
-                    case 2:
-                        dotw = "Tuesday";
-                        break;
-                    case 3:
-                        dotw = "Wednesday";
-                        break;
-                    case 4:
-                        dotw = "Thursday";
-                        break;
-                    case 5:
-                        dotw = "Friday";
-                        break;
-                    case 6:
-                        dotw = "Saturday";
-                        break;
-                    default:
-                        dotw = this.props.forecast.daily.data[1].time;
-                        break;
-                }
+    };
+    */
+    render() {
+        var dotw = "";
+        var vDaily = this.props.forecast.daily;
+        var output = "";
+        
+        for (var i = 0; i <= vDaily.data.length; i++) {                              // Guide: https://www.youtube.com/watch?v=HWdLisBrlV8 array at 13:30
+      //      for (var key in vDaily.data[i]) {
+      //          if (vDaily.data[i].hasOwnProperty(key)) {
+                    if (i === 0) {
+                        dotw = "Today";
+                    } else {
+                        switch(new Date(vDaily.data[i].time*1000).getDay()){
+                            case 0: 
+                                dotw = "Sunday";
+                                break;
+                            case 1:
+                                dotw = "Monday";
+                                break;
+                            case 2:
+                                dotw = "Tuesday";
+                                break;
+                            case 3:
+                                dotw = "Wednesday";
+                                break;
+                            case 4:
+                                dotw = "Thursday";
+                                break;
+                            case 5:
+                                dotw = "Friday";
+                                break;
+                            case 6:
+                                dotw = "Saturday";
+                                break;
+                            default:
+                                dotw = "Today";
+                                break;
+                        }
+                    }
+                                        
+                    // .data[i][key]
+                    output += <Tile heading={dotw} attr1={Math.round(vDaily.data[i].temperatureMax)} attr2={Math.round(vDaily.data[i].temperatureMin)} attr3={Math.round(vDaily.data[i].windSpeed)} attr4={vDaily.data[i].precipProbability*100} attr5="None" />;
+      //          }   // hasOwnProperty check
+      //      }       //for each object
+        }           //for each array element
+console.log(output);
+        
+
+
+
         return(
             <div className="container">
                 <h3>Course Forecast for: {this.props.name}</h3><h4>Bowling Green, OH</h4>
-                <Tile heading="Today" attr1="75 degrees" attr2="5-10 mph winds" attr3="20% chance of rain" attr4="Course Condition: Muddy" />
+                {output}
+                {/*<Tile heading="Today" attr1={Math.round(vDaily.data[0].temperatureMax)} attr2={Math.round(vDaily.data[0].temperatureMin)} attr3={Math.round(vDaily.data[0].windSpeed)} attr4={vDaily.data[0].precipProbability*100} attr5="None" />
                 <Tile heading={dotw} attr1="77 degrees" attr2="10-14 mph winds" attr3="15% chance of rain" attr4="Course Condition: Partially Flooded" />
                 <Tile heading="Sunday" attr1="79 degrees" attr2="3-8 mph winds" attr3="25% chance of rain" attr4="Course Condition: Muddy" />
                 <Tile heading="Monday" attr1="77 degrees" attr2="6-12 mph winds" attr3="40% chance of rain" attr4="Course Condition: Muddy" />
                 <Tile heading="Tuesday" attr1="77 degrees" attr2="6-12 mph winds" attr3="40% chance of rain" attr4="Course Condition: Muddy" />
                 <Tile heading="Wednesday" attr1="77 degrees" attr2="6-12 mph winds" attr3="40% chance of rain" attr4="Course Condition: Muddy" />
                 <Tile heading="Thursday" attr1="77 degrees" attr2="6-12 mph winds" attr3="40% chance of rain" attr4="Course Condition: Muddy" />
+                */}
             </div>
         );
     }
@@ -132,12 +155,12 @@ class DSWeekly extends React.Component {
 
 function Tile(props) {
   return (
-    <div className="tile">         {/* Cleanup Steps: could convert the attributes into a loop */}
-        <h3>{props.heading}</h3>    {/* tile heading: for dg-weather = course name in playable forecast, otherwise blank as the course name is displayed elsewhere (parent usually) */}
-        <p>{props.attr1}</p>        {/* Attribute 1: for dg-weather = temperature */}
-        <p>{props.attr2}</p>        {/* Attribute 2: for dg-weather = wind */}
-        <p>{props.attr3}</p>        {/* Attribute 3: for dg-weather = % chance of rain for the day */}
-        <p>{props.attr4}</p>        {/* Attribute 4: for dg-weather = any community generated alerts for that course, ie. holes are flooded, course is muddy/snow covered, etc. */}
+    <div className="tile">                                  {/* Cleanup Steps: could convert the attributes into a loop */}
+        <h3>{props.heading}</h3>                            {/* tile heading: for dg-weather = course name in playable forecast, otherwise blank as the course name is displayed elsewhere (parent usually) */}
+        <p>High: {props.attr1} Low: {props.attr2}</p>       {/* Attribute 1 & 2: for dg-weather = Hi/Low temperature */}
+        <p>{props.attr3} mph</p>                            {/* Attribute 3: for dg-weather = wind */}
+        <p>{props.attr4}% chance of rain</p>                {/* Attribute 4: for dg-weather = % chance of rain for the day */}
+        <p>Alerts: {props.attr5}</p>                        {/* Attribute 5: for dg-weather = any community generated alerts for that course, ie. holes are flooded, course is muddy/snow covered, etc. */}
     </div>
   );
 }
