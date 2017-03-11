47c74b2b334458974cfa1a2d80095c96e8c7e5c8
Howie17 <Howie17@gmail.com>
2017-03-11 11:54:56 -0500

    Minot updates

--- client/src/pages/DSForecast.js
+++ client/src/pages/DSForecast.js
@@ -6,7 +6,6 @@ class DSForecast extends React.Component {
         return(
             <div>
                 <DSSearch />
-
             </div>
         );
     }
@@ -52,7 +51,7 @@ class DSSearch extends React.Component {
             displayHourly = '';
         if (this.state.isSearchClick) {
             displayWeekly = <DSWeekly name={this.state.value} forecast={this.state.forecast}/>;         //todo: name should equal the json id returned
-            displayHourly = <DSHourly />;                                 
+            displayHourly = <DSHourly forecast={this.state.forecast}/>;                                 
         } else {
             displayWeekly = '';
             displayHourly = '';
@@ -103,7 +102,7 @@ class DSWeekly extends React.Component {
                 <h3>Course Forecast for: {this.props.name}</h3><h4>City, State</h4>         {/* todo: this.props.name shouldn't update based on text input, but what is returned via json */}
                 {vDaily.data.map((day, index)=> (                                           /* todo: benign error, this line is called before forecast is populated, throwing an uncaught undefined error. */
                     <Tile
-                        key={index} // Needed when returning a list of components in a loop
+                        key={index}                                                         // Needed when returning a list of components in a loop
                         heading={index === 0 ? "Today" : this.getDayOfTheWeek(day)}
                         attr1={Math.round(day.temperatureMax)}
                         attr2={Math.round(day.temperatureMin)}
@@ -120,11 +119,11 @@ class DSWeekly extends React.Component {
 function Tile(props) {
   return (
         <div className="tile">                                  
-            <h3>{props.heading}</h3>                            {/* tile heading: for dg-weather = course name in playable forecast, otherwise blank as the course name is displayed elsewhere (parent usually) */}
-            <p>High: {props.attr1} Low: {props.attr2}</p>       {/* Attribute 1 & 2: for dg-weather = Hi/Low temperature */}
-            <p>{props.attr3} mph</p>                            {/* Attribute 3: for dg-weather = wind */}
-            <p>{props.attr4}% chance of rain</p>                {/* Attribute 4: for dg-weather = % chance of rain for the day */}
-            <p>Alerts: {props.attr5}</p>                        {/* Attribute 5: for dg-weather = any community generated alerts for that course, ie. holes are flooded, course is muddy/snow covered, etc. */}
+            <h3>{props.heading}</h3>                                                {/* tile heading: for dg-weather = course name in playable forecast, otherwise blank as the course name is displayed elsewhere (parent usually) */}
+            <p>High: {props.attr1}&#x2109; Low: {props.attr2}&#x2109;</p>         {/* Attribute 1 & 2: for dg-weather = Hi/Low temperature */}
+            <p>{props.attr3} mph</p>                                                {/* Attribute 3: for dg-weather = wind */}
+            <p>{props.attr4}% chance of rain</p>                                    {/* Attribute 4: for dg-weather = % chance of rain for the day */}
+            <p>Alerts: {props.attr5}</p>                                            {/* Attribute 5: for dg-weather = any community generated alerts for that course, ie. holes are flooded, course is muddy/snow covered, etc. */}
         </div>
   );
 }
@@ -133,13 +132,23 @@ class DSHourly extends React.Component {
 
     /*
     getMornNight(hour) {
-        switch((new Date(hour.time * 1000)).getDay()){
-            case 0: 
-                return "AM";
+        switch((new Date(hour.time * 1000)).getHours()){
+            case 0:
             case 1:
-                return "PM";
+            case 2:
+            case 3:
+            case 4:
+            case 5:
+            case 6:
+            case 7:
+            case 8:
+            case 9:
+            case 10:
+            case 11:
+            case 12: 
+                return "AM";
             default:
-                return "";
+                return "PM";
         }
     }
     */
@@ -175,8 +184,8 @@ function Row(props) {
             <p> 
                 {props.attr1}(AM/PM) | 
                 {props.attr8} | 
-                Temp:{props.attr2} | 
-                Feels: {props.attr3} | 
+                Temp:{props.attr2} &#x2109; | 
+                Feels: {props.attr3} &#x2109; | 
                 Wind: {props.attr4} mph | 
                 {props.attr5 } % chance of rain | 
                 {props.attr6} rainfall(in) | 
