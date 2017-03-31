183015b7fd7d99a8ecfdc9ef494616a75b7c3f45
howie17 <Howie17@gmail.com>
2017-03-31 11:49:56 -0400

    Minor styling update

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import '../Nav.css';
 
+const styles = {
+    Footer: {
+        "paddingTop": "20px",
+    },
+}
+
 class Footer extends React.Component {
     render() {
 
         return (
-            <div className="Footer">
+            <div className="Footer" style={styles.Footer}>
                 <p>Powered by <a href="https://darksky.net/poweredby/">Dark Sky</a>.</p>
             </div>
         );
